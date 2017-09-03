65a08f3284873ac3869085934362691002b8e813
Michael <michael.ginart@gmail.com>
2017-09-03 18:50:28 +0200

    Added comments for clarification to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch');
 
+/* -------------------------
+DEFAULT Task (RUNS DEFAULT BY CMD GULP IN CMD)
+-------------------------- */
 gulp.task('default', function(){
 console.log("Hooray - you created a gulp task.");
 });
 
+/* -------------------------
+HTML Task -
+-------------------------- */
 gulp.task('html', function(){
   console.log("imagine something useful to your html here");
 });
-
+/* -------------------------
+CSS Styles Task -
+-------------------------- */
 gulp.task('styles', function(){
   console.log("Imagine Sass or PostCSS tasks running here.");
 });
 
+/* -------------------------
+Watch Task - Starts a watcher for changes on listed files 
+-------------------------- */
 gulp.task('watch', function(){
   watch('./app/index.html', function(){
     gulp.start('html');
